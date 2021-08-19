af0ed30cf2924c581109125126c08da05a210979
ImGabreuw <imgabreuw.dev@gmail.com>
2021-08-19 15:00:10 -0300

    Refactored code

--- src/main/java/com/giacom/java/kubernetes/JavaKubernetesApplication.java
+++ src/main/java/com/giacom/java/kubernetes/JavaKubernetesApplication.java
@@ -28,19 +28,26 @@ public class JavaKubernetesApplication {
 
     @PostConstruct
     public void checkIfWorks() {
-
         repository.deleteAll();
 
-        service.create(new User("Minikube",
-                LocalDate.of(2006, 10, 01)));
-        service.create(new User("Kubectl",
-                LocalDate.of(1999, 05, 15)));
+        service.create(
+                new User(
+                        "Minikube",
+                        LocalDate.of(2006, 10, 01)
+                )
+        );
+        service.create(
+                new User(
+                        "Kubectl",
+                        LocalDate.of(1999, 05, 15)
+                )
+        );
 
         List<User> findAll = service.findAll();
+
         for (User user : findAll) {
             System.out.println(user.getId() + ":" + user.getName());
         }
-
     }
 
 }
